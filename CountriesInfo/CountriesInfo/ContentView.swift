97665a93093b861bb8b37960221bb0fe67291a28
Dmitrii Vaniushkin <davanyushkin@gmail.com>
2024-09-13 23:02:50 +0200

    Create empty project

---
+++ CountriesInfo/CountriesInfo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountriesInfo
+//
+//  Created by Dmitrii Vaniushkin on 13.9.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountriesInfo/CountriesInfo/CountriesInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesInfoApp.swift
+//  CountriesInfo
+//
+//  Created by Dmitrii Vaniushkin on 13.9.24..
+//
+
+import SwiftUI
+
+@main
+struct CountriesInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
